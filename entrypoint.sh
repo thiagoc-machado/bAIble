426aa5d7c30ec18f9cff3cd21fc0e7c9aeaa5018
Thiagoc.machado <thiagocmachado@yahoo.com.br>
2025-05-05 07:36:36 +0200

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,8 @@ python manage.py migrate --noinput
 echo "Coletando arquivos estáticos..."
 # python manage.py collectstatic --noinput
 
+echo "⏬ Baixando modelo offline se necessário..."
+python download_model.py
+
 echo "Iniciando servidor Gunicorn..."
 exec gunicorn baible.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --timeout 120
